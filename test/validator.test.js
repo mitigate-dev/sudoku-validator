0f48838f1b1e95d7826a30fd5638bb43c3d335b6
Pauls Liepa <liepauls@gmail.com>
2022-03-25 18:18:11 +0200

    Add test scenario titles

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,27 +1,27 @@
 const Validator = require('../src/validator')
 const fs = require('fs').promises
 
-describe('it recognizes valid sudoku', () => {
-  test('', async () => {
+describe('valid sudoku', () => {
+  test('recognizes completed sudoku', async () => {
     const sudoku = await fs.readFile(__dirname + '/fixtures/valid_complete.sudoku')
 
     expect(Validator.validate(sudoku.toString())).toBe('Sudoku is valid.')
   })
 
-  test('', async () => {
+  test('recognizes incompleted sudoku', async () => {
     const sudoku = await fs.readFile(__dirname + '/fixtures/valid_incomplete.sudoku')
 
     expect(Validator.validate(sudoku.toString())).toBe('Sudoku is valid.')
   })
 })
 
-describe('it recognizes invalid sudoku', () => {
+describe('invalid sudoku', () => {
   [
     __dirname + 'fixtures/invalid_due_to_row_dupe.sudoku',
     __dirname + 'fixtures/invalid_due_to_column_dupe.sudoku',
     __dirname + 'fixtures/invalid_due_to_subgroup_dupe.sudoku'
   ].forEach(path => {
-    test('', async () => {
+    test('recognizes invalid sudoku', async () => {
       const sudoku = await fs.readFile(path)
 
       expect(Validator.validate(sudoku.toString())).toBe('Sudoku is invalid.')
